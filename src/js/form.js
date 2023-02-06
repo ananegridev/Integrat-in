ea1687e952d37640ee9aed35ae4d07a8b77dbad9
Ana Paula Negri <anywtf@outlook.com>
2023-02-06 11:21:39 -0300

    add code

--- src/js/form.js
+++ src/js/form.js
@@ -1,34 +1,21 @@
 function formStart(){
     let nameValue = document.querySelector(".js-input-name").value;
     let companyValue = document.querySelector(".js-input-company").value;
-    let workValue = document.querySelector(".js-input-work").value;
+    let typeCompanyValue = document.querySelector(".js-input-typecompany").value;
+    let companyNameValue = document.querySelector(".js-input-companyname").value;
+    let departamentValue = document.querySelector(".js-input-departament").value;
   
     let formPerson = {
         name: nameValue,
         company: companyValue,
-        work: workValue
+        newCompany: typeCompanyValue,
+        companyName: companyNameValue,
+        departament: departamentValue
     };
 
     localStorage.getItem(formPerson);
     return true;
   }
-
-
-//    const field = document.querySelector('input');
-
-
-
-//   field.addEventListener('input', () => {
-//     field.setCustomValidity('');
-//     field.checkValidity();
-//       console.log(field.checkValidity());
-  
-//   });
-  
-//   field.addEventListener('invalid', () => {
-//     field.setCustomValidity('This Field Has Errors');
-//   })
-
 
 function nextPage(){
     window.location.href = "/quiz.html"
