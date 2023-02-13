29a75a1d9ebf1d5ba75dfbc4e432a9f9321638dc
Ana Paula Negri <anywtf@outlook.com>
2023-02-13 11:39:24 -0300

    alteracao form

--- src/js/form.js
+++ src/js/form.js
@@ -3,7 +3,7 @@ function formStart(){
     let emailValue = document.querySelector(".js-input-email").value;
     let companyValue = document.querySelector(".js-input-company").value;
     let companyNameValue = document.querySelector(".js-input-companyname").value;
-    let cnpjValue = document.querySelector(".js-input-cnpj").value;
+    let domainValue = document.querySelector(".js-input-domain").value;
     let departamentValue = document.querySelector(".js-input-departament").value;
     let sectorValue = document.querySelector(".js-input-sector").value;
 
@@ -12,7 +12,7 @@ function formStart(){
         email: emailValue,
         company: companyValue,
         companyName: companyNameValue,
-        cnpj: cnpjValue,
+        domain: domainValue,
         departament: departamentValue,
         sector: sectorValue,
     };
